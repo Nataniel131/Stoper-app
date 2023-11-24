301c78e5bbcf433784c3b0b534c2de4ee8bf68f7
Nataniel Rutkowski <rutkowski.nataniel@gmail.com>
2023-11-24 14:22:06 +0100

    app

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import FormattedTime from "./components/FormattedTime/FormattedTime";
+import Button from "./components/Buttons/Button";
+import Container from "./components/Container/Container";
+import { useEffect, useState } from 'react';
+
+const App = () => {
+
+  const [timer, setTimer] = useState(null);
+  const [time, setTime] = useState(0)
+
+  const startTimer = () => {
+    setTimer(setInterval(() => {
+      setTime(prevValue => prevValue + 1);
+    }, 1))
+  };
+
+  const stopTimer = () => {
+    clearInterval(timer);
+  };
+
+  const resetTimer = () => {
+    stopTimer();
+    setTime(0);
+  };
+
+  useEffect(() => {
+    return () => {
+      if(timer) clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <Container>
+      <FormattedTime milliseconds={time} />
+      <Button onClick={startTimer}>start</Button>
+      <Button onClick={stopTimer}>stop</Button>
+      <Button onClick={resetTimer}>reset</Button>
+    </Container>
+  );
+}
+
+export default App;
